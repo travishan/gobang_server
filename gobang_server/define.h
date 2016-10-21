c134738790d4bd7b3ebf24519dd90c7a7ee28fd0
Travis <Travis@DESKTOP-LU781MQ>
2016-10-21 09:41:26 +0800

    simple server

---
+++ gobang_server/define.h
@@ -0,0 +1,42 @@
+#ifndef __DEFINE__H__
+#define __DEFINE__H__
+
+#ifdef _WIN32
+	#pragma once
+	#include <WinSock2.h>
+	#include <WS2tcpip.h>
+	//#pragma comment(lib,"ws2_32.lib")
+	#pragma comment(lib,"WS2_32.lib") 
+#else
+	#include <sys/types.h>
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <unistd.h>
+	#include <netdb.h>
+	#include <arpa/inet.h>
+	
+#endif // WIN32
+
+
+#ifdef _WIN32
+	WSADATA g_wsadata;
+	#define CloseSocket closesocket
+	#define GetSocketError WSAGetLastError
+	#define StartSocketLib WSAStartup( MAKEWORD(2,2), &g_wsadata);
+	#define CloseSocketLib WSACleanup();
+	#ifndef socklen_t
+		typedef int socklen_t;
+	#endif // !socklen_t
+
+#else
+	#define CloseSocket close
+	#define GetSocketError errno
+	#define StartSocketLib {}
+	#define CloseSocketLib {}
+#endif // _WIN32
+
+
+
+
+
+#endif // !__DEFINE__H__
---
+++ gobang_server/main.cpp
@@ -0,0 +1,49 @@
+#include "define.h"
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int err;
+	StartSocketLib;
+
+	int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == -1) {
+		cout << "Socket creation error!" << endl;
+		return 0;
+	}
+	cout << "Socket created!" << endl;
+
+	struct sockaddr_in socketaddress;
+	socklen_t sa_size = sizeof(struct sockaddr_in);
+	socketaddress.sin_family = AF_INET;
+	socketaddress.sin_port = htons(6000);
+	socketaddress.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	memset(&(socketaddress.sin_zero), 0, 8);
+
+	//bind
+	err = bind(sock, (struct sockaddr*)&socketaddress, sa_size);
+
+	err = listen(sock, 16);
+
+	int datasock;
+	datasock = accept(sock, (struct sockaddr*)&socketaddress, &sa_size);
+
+	char buffer[128];
+	err = recv(datasock, buffer, 128, 0);
+
+	cout << "Data received:" << endl;
+	cout << buffer << endl;
+
+	shutdown(datasock, 2);
+	CloseSocket(datasock);
+
+	shutdown(sock, 2);
+
+	CloseSocket(sock);
+
+	CloseSocketLib;
+	return 0;
+}
