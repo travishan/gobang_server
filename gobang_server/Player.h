5c129a7b905346d9e6957c17ebca6fabf6e2d978
Travis <Travis@DESKTOP-LU781MQ>
2016-10-23 23:51:53 +0800

    添加Player类和Room类

---
+++ gobang_server/Player.h
@@ -0,0 +1,31 @@
+#ifndef __PLAYER__H__
+#define __PLAYER__H__
+
+
+
+/************************
+
+Player类
+保存玩家的Socket
+下过的棋子
+名字
+
+
+
+************************/
+
+
+
+class Player
+{
+
+
+};
+
+
+
+
+
+
+#endif // !__PLAYER__H__
+
--- gobang_server/ServerSocket.h
+++ gobang_server/ServerSocket.h
@@ -39,19 +39,28 @@ public:
 	
 
 private:
+	//表示服务器是否运行的变量
 	int running;
+	//下一个客户端连进来时的index
 	int next_ind;
+	//保存服务器的socket变量
 	TCPsocket server_socket;
+	//客户端数组， clients和sockets一一对应
 	Client clients[MAX_SOCKETS];
 	SDLNet_SocketSet socket_set;
 	TCPsocket sockets[MAX_SOCKETS];
 
 
 private:
+	//初始化
 	void init();
+	//当有socket流入时调用
 	int acceptSocket(int index);
+	//关闭索引为index的socket
 	void closeSocket(int index);
+	//接收数据
 	uint8_t* recvData(int index, uint16_t &length);
+	//关闭所有socket
 	void close();
 
 };
